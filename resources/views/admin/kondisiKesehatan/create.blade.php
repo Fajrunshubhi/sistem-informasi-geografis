323f4480d5a9eaa88f51a3fe73e27a8b8f9fd8e9
Fajrun Shubhi <fajrunss7@gmail.com>
2024-10-01 19:44:56 +0700

    modif leaflet

--- resources/views/admin/kondisiKesehatan/create.blade.php
+++ resources/views/admin/kondisiKesehatan/create.blade.php
@@ -130,26 +130,24 @@
         });
     });
 
-
+    // Buat map
+    let map = L.map('map', {
+        center: [-7.824893908172728, 109.96604338891359],
+        zoom: 15,
+    });
+    // Map 1
     let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
+    }).addTo(map);
+    // Map 2
     let osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'
     });
 
     const key = 'KkGDVJv1GsVinRe0vQUy';
-    let mapTilerSatelit = L.tileLayer(`https://api.maptiler.com/maps/satellite/?key=${key}`,{
-        tileSize: 512,
-        zoomOffset: -1,
-        minZoom: 1,
-        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-        crossOrigin: true
-    });
-    // https://api.maptiler.com/maps/streets-v2/style.json?key=KkGDVJv1GsVinRe0vQUy
-    // https://api.maptiler.com/maps/streets-v2/tiles.json?key=KkGDVJv1GsVinRe0vQUy
+    // Map 3
     let mapTilerStreet = L.tileLayer(`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}`,{
         tileSize: 512,
         zoomOffset: -1,
@@ -157,21 +155,22 @@
         attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
         crossOrigin: true
     });
+    // Map 4 (err)
+    let mapTilerSatelit = L.tileLayer(`https://api.maptiler.com/tiles/satellite-v2/{z}/{x}/{y}.png?key=${key}`,{ //style URL
+        tileSize: 512,
+        zoomOffset: -1,
+        minZoom: 1,
+        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
+        crossOrigin: true
+      });
     
-
-    let desa = L.layerGroup();
-
-    let map = L.map('map', {
-        center: [-7.824893908172728, 109.96604338891359],
-        zoom: 15,
-        layers: [osm]
-    });
-    
-    
+    // Marker untuk ambil nilai latitude longitude
     let marker = L.marker([-7.824893908172728, 109.96604338891359], {
         draggable: true,
         clickable: true
     }).addTo(map);
+
+    // Lokasi sekarang 
     L.control.locate({
         locateOptions: {
             maxZoom: 19,
@@ -180,9 +179,9 @@
         strings: {
             title: "Anda di sini!"
         }
-        })
-        .addTo(map);
+    }).addTo(map);
 
+    // Mengambil nilai latitude longitude
     marker.on('dragend', function (e) {
         let position = marker.getLatLng();
         document.getElementById('latitude').value = position.lat;
@@ -194,16 +193,16 @@
         document.getElementById('latitude').value = e.latlng.lat;
         document.getElementById('longitude').value = e.latlng.lng;
     });
-        let overLayer = {
-            "desa" : desa
-        };
-        let baseMaps = {
-            "OpenStreetMap": osm,
-            "OpenStreetMap.HOT": osmHOT,
-            "MapTilerSatelit": mapTilerSatelit,
-            "MapTilerStreet": mapTilerStreet
-        };
-        L.control.layers(baseMaps, overLayer).addTo(map);
+
+    // List map
+    let baseMaps = {
+        "OpenStreetMap": osm,
+        "OpenStreetMap.HOT": osmHOT,
+        "MapTilerStreet": mapTilerStreet,
+        "MapTilerSatelit": mapTilerSatelit,
+    };
+
+    L.control.layers(baseMaps).addTo(map);
 
 </script>
 @endpush
--- resources/views/admin/pusatKesehatan/create.blade.php
+++ resources/views/admin/pusatKesehatan/create.blade.php
@@ -161,35 +161,6 @@
         }
     }
 
-
-    let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    });
-    let osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'
-    });
-
-    const key = 'KkGDVJv1GsVinRe0vQUy';
-    let mapTilerSatelit = L.tileLayer(`https://api.maptiler.com/maps/satellite/?key=${key}`,{
-        tileSize: 512,
-        zoomOffset: -1,
-        minZoom: 1,
-        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-        crossOrigin: true
-    });
-    // https://api.maptiler.com/maps/streets-v2/style.json?key=KkGDVJv1GsVinRe0vQUy
-    // https://api.maptiler.com/maps/streets-v2/tiles.json?key=KkGDVJv1GsVinRe0vQUy
-    let mapTilerStreet = L.tileLayer(`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}`,{
-        tileSize: 512,
-        zoomOffset: -1,
-        minZoom: 1,
-        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
-        crossOrigin: true
-    });
-    
-
     // Akses lokasi pengguna manual
     // if (navigator.geolocation) {
     //     navigator.geolocation.getCurrentPosition(function(position) {
@@ -213,14 +184,48 @@
     // } else {
     //     alert("Geolocation is not supported by this browser.");
     // }
-    
-    let desa = L.layerGroup();
-
+   
+    // Buat map
     let map = L.map('map', {
         center: [-7.824893908172728, 109.96604338891359],
         zoom: 15,
-        layers: [osm]
     });
+    // Map 1
+    let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    // Map 2
+    let osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'
+    });
+
+    const key = 'KkGDVJv1GsVinRe0vQUy';
+    // Map 3
+    let mapTilerStreet = L.tileLayer(`https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=${key}`,{
+        tileSize: 512,
+        zoomOffset: -1,
+        minZoom: 1,
+        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
+        crossOrigin: true
+    });
+    // Map 4 (err)
+    let mapTilerSatelit = L.tileLayer(`https://api.maptiler.com/tiles/satellite-v2/{z}/{x}/{y}.png?key=${key}`,{ //style URL
+        tileSize: 512,
+        zoomOffset: -1,
+        minZoom: 1,
+        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
+        crossOrigin: true
+      });
+    
+    // Marker untuk ambil nilai latitude longitude
+    let marker = L.marker([-7.824893908172728, 109.96604338891359], {
+        draggable: true,
+        clickable: true
+    }).addTo(map);
+
+    // Lokasi sekarang 
     L.control.locate({
         locateOptions: {
             maxZoom: 19,
@@ -229,15 +234,9 @@
         strings: {
             title: "Anda di sini!"
         }
-        })
-        .addTo(map);
-    
-    
-        let marker = L.marker([-7.824893908172728, 109.96604338891359], {
-        draggable: true,
-        clickable: true
     }).addTo(map);
 
+    // Mengambil nilai latitude longitude
     marker.on('dragend', function (e) {
         let position = marker.getLatLng();
         document.getElementById('latitude').value = position.lat;
@@ -249,16 +248,16 @@
         document.getElementById('latitude').value = e.latlng.lat;
         document.getElementById('longitude').value = e.latlng.lng;
     });
-    let overLayer = {
-        "desa" : desa
-    };
+
+    // List map
     let baseMaps = {
         "OpenStreetMap": osm,
         "OpenStreetMap.HOT": osmHOT,
+        "MapTilerStreet": mapTilerStreet,
         "MapTilerSatelit": mapTilerSatelit,
-        "MapTilerStreet": mapTilerStreet
     };
-    L.control.layers(baseMaps, overLayer).addTo(map);
+
+    L.control.layers(baseMaps).addTo(map);
 
 </script>
 @endpush
